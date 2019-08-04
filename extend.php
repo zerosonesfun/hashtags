697238895c2df3cd7e2912caa4993ca91bf48678
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-08-04 01:11:23 -0400

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,8 +1,9 @@
 <?php
 
 /*
- * This file is part of an extension for Flarum, called Fickle.
- * The creator of Fickle is Billy Wilcosky. Connect with Billy at https://wilcosky.com.
+ * This file is part of an extension for Flarum, called Hashtags.
+ * The idea behind Hashtags is from Billy Wilcosky. Connect with Billy at https://wilcosky.com.
+ * But, it would not have been possible without Carl Winkelman. https://clarkwinkelmann.com/.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -11,8 +12,29 @@ use Flarum\Extend;
 use Flarum\Frontend\Document;
 
 return [
-    (new Extend\Frontend('forum'))
+    // Register extenders here to customize your forum!
+(new Extend\Frontend('forum'))
         ->content(function (Document $document) {
-            $document->head[] = '<script defer src="/assets/extensions/zerosonesfun-fickle-announcement/fickle.js"></script>';
+            $document->foot[] = <<<HTML
+<script>
+  flarum.core.compat.extend.extend(flarum.core.compat['components/CommentPost'].prototype, 'config', function(output, isInitialized, context) {
+    // context.contentHtml was updated by CommentPost.config() so we can simply compare it again
+    if (context.customExtLastContentHtml !== context.contentHtml) {
+      var siteURL = '',
+        entries = this.element.querySelectorAll('.Post-body p'),
+        i;console.log(entries);
+  
+      if ( entries.length > 0 ) {
+        for (i = 0; i < entries.length; i = i + 1) {
+          entries[i].innerHTML = entries[i].innerHTML.replace(/#(\S+)/g,'<a href="/?q=$1" title="Find more posts tagged with #$1">#$1</a>');
+        }
+      }
+    }
+
+    // We store the new value in our own custom variable so we know if it changes on the next redraw
+    context.customExtLastContentHtml = context.contentHtml;
+  });
+</script>
+HTML;
         })
 ];
