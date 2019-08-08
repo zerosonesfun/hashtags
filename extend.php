b28da3b7885c8ed6d9fc59a57b78b0a802c07601
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-08-07 23:27:28 -0400

    Update extend.php
    Fixed conflict with post mentions.

--- extend.php
+++ extend.php
@@ -2,8 +2,8 @@
 
 /*
  * This file is part of an extension for Flarum, called Hashtags.
- * The idea behind Hashtags is from Billy Wilcosky. Connect with Billy at https://wilcosky.com.
- * But, it would not have been possible without Clark Winkelman who showed how to hook into posts. https://clarkwinkelmann.com/.
+ * The creator of this extension is Billy Wilcosky.
+ * Connect with Billy at https://wilcosky.com.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -12,26 +12,31 @@ use Flarum\Extend;
 use Flarum\Frontend\Document;
 
 return [
-    // Register extenders here to customize your forum!
 (new Extend\Frontend('forum'))
         ->content(function (Document $document) {
             $document->foot[] = <<<HTML
+<script type="text/javascript" src="/assets/extensions/zerosonesfun-hashtags/purify.min.js"></script>
 <script>
   flarum.core.compat.extend.extend(flarum.core.compat['components/CommentPost'].prototype, 'config', function(output, isInitialized, context) {
-    // context.contentHtml was updated by CommentPost.config() so we can simply compare it again
     if (context.customExtLastContentHtml !== context.contentHtml) {
+    var els = document.getElementsByTagName("p");
+    for(var i = 0, all = els.length; i < all; i++){   
+         els[i].classList.add('Post-paragraph');
+     }
+    var elss = document.getElementsByClassName("PostMention");
+    for(var i = 0, all = elss.length; i < all; i++){   
+         elss[i].parentNode.classList.remove('Post-paragraph');
+     }
       var siteURL = '/',
-        entries = this.element.querySelectorAll('.Post-body p'),
+        entries = this.element.querySelectorAll('.Post-paragraph'),
         i;console.log(entries);
   
       if ( entries.length > 0 ) {
         for (i = 0; i < entries.length; i = i + 1) {
-          entries[i].innerHTML = entries[i].innerHTML.replace(/((?!([\S])).|^)#(\S+)\b/g,' <a href="'+siteURL+'?q=$3" title="Find more posts tagged with #$3">#$3</a>');
+          entries[i].innerHTML = entries[i].innerHTML.replace(/((?!([\S])).|^)#(\S+)\b/g,DOMPurify.sanitize(' <a href="'+siteURL+'?q=$3" title="Find more posts related to $3">#$3</a>'));
         }
       }
     }
-
-    // We store the new value in our own custom variable so we know if it changes on the next redraw
     context.customExtLastContentHtml = context.contentHtml;
   });
 </script>
