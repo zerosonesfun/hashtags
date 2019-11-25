03196dc78f223267353e207257dc29ebcedbb58f
zerosonesfun <41351317+zerosonesfun@users.noreply.github.com>
2019-11-25 11:41:43 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -2,8 +2,6 @@
 
 /*
  * This file is part of an extension for Flarum, called Hashtags.
- * The creator of this extension is Billy Wilcosky.
- * Connect with Billy at https://wilcosky.com.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
